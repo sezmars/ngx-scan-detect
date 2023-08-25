86d75fe6b4c5bccc29595c342f257b068a7d704b
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-25 19:35:53 +0300

    refactor: clear

--- projects/ngx-barcodeput/src/lib/ngx-barcodeput.module.ts
+++
@@ -1,8 +0,0 @@
-import {NgModule} from '@angular/core';
-import {NgxBarCodePutDirective} from './directive/ngx-barcodeput.directive';
-
-@NgModule({
-  declarations: [NgxBarCodePutDirective],
-  exports: [NgxBarCodePutDirective],
-})
-export class NgxBarCodePutModule {}
--- src/app/examples/ngx-barcodeput-lib/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './ngx-barcodeput-lib.component';
