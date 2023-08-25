778819f51c459f0ac1b2cffa29d690aae4585420
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-26 00:04:52 +0300

    refactor: fix test

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    testIsolation: false,
   },
 
   component: {
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,13 @@
-describe('My First Test', () => {
+describe('Initial Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
-    cy.contains('app is running!');
+    cy.contains('Welcome to Barcode Scanner Input Detect!');
+  });
+});
+
+describe('Input Test', () => {
+  it('Visits the initial project page', () => {
+    cy.visit('/');
+    cy.get('input').type('https://sezmars.github.io/ngx-barcodeput');
   });
 });
